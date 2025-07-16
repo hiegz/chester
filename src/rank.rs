6100f1fd55a1db31affc9f1aad154234dcfab42e
Volodymyr But <git@hiegz.com>
2025-07-16 13:41:40 +0200

    enumerate ranks

--- src/rank.rs
+++ src/rank.rs
@@ -1,5 +1,14 @@
 pub type Rank = u8;
 
+pub const ONE: Rank = 0;
+pub const TWO: Rank = 1;
+pub const THREE: Rank = 2;
+pub const FOUR: Rank = 3;
+pub const FIVE: Rank = 4;
+pub const SIX: Rank = 5;
+pub const SEVEN: Rank = 6;
+pub const EIGHT: Rank = 7;
+
 /// Wraps the given rank to be represented by a debug formatter.
 #[derive(Eq, PartialEq)]
 pub struct RankWrapper(pub Rank);
