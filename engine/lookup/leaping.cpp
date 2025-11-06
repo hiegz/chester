d5743ada9ebbe1bdc50fbdfdea596871643b9f00
Volodymyr But <git@hiegz.com>
2025-11-06 21:04:48 +0100

    merge engine/lookup/king.cpp and engine/lookup/knight.cpp

--- engine/lookup/leaping.cpp
+++ engine/lookup/leaping.cpp
@@ -19,7 +19,87 @@ using chester::engine::square;
 
 namespace {
 
-const std::size_t SQUARES = 64;
+constexpr std::size_t SQUARES = 64;
+
+namespace king {
+
+/** lookup table for pseudo-legal moves of a kin*/
+class table {
+  public:
+    std::array<chester::engine::bitboard, SQUARES> cells;
+
+    table() : cells() {
+        for (std::uint8_t i = 0; i < SQUARES; ++i) {
+            const auto square = (class square)(enum square::value)i;
+            const auto file   = square.file();
+            const auto rank   = square.rank();
+
+            auto moves = bitboard::empty();
+
+            // . x .
+            // . K .
+            // . . .
+            if (rank < rank::eight) {
+                moves |= square >> 8;
+            }
+
+            // . . x
+            // . K .
+            // . . .
+            if (file < file::h and rank < rank::eight) {
+                moves |= square >> 9;
+            }
+
+            // . . .
+            // . K x
+            // . . .
+            if (file < file::h) {
+                moves |= square >> 1;
+            }
+
+            // . . .
+            // . K .
+            // . . x
+            if (file < file::h and rank > rank::one) {
+                moves |= square << 7;
+            }
+
+            // . . .
+            // . K .
+            // . x .
+            if (rank > rank::one) {
+                moves |= square << 8;
+            }
+
+            // . . .
+            // . K .
+            // x . .
+            if (file > file::a and rank > rank::one) {
+                moves |= square << 9;
+            }
+
+            // . . .
+            // x K .
+            // . . .
+            if (file > file::a) {
+                moves |= square << 1;
+            }
+
+            // x . .
+            // . K .
+            // . . .
+            if (file > file::a and rank < rank::eight) {
+                moves |= square >> 7;
+            }
+
+            this->cells[i] = moves;
+        }
+    }
+};
+
+} // namespace king
+
+namespace knight {
 
 /** lookup table for pseudo-legal moves of a knight */
 class table {
@@ -111,12 +191,21 @@ class table {
     }
 };
 
+} // namespace knight
+
 } // namespace
+
+template <>
+auto chester::engine::lookup<piece::king>(square square)
+    -> chester::engine::bitboard {
+    static ::king::table table;
+    return table.cells[square.value];
+}
 
 template <>
 auto chester::engine::lookup<piece::knight>(square square)
     -> chester::engine::bitboard {
-    static ::table table;
+    static ::knight::table table;
     return table.cells[square.value];
 }
 
