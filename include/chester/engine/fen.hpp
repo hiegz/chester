a074ce1255b7457e8d30cb661ac61e99c936e322
Volodymyr But <git@hiegz.com>
2025-11-13 10:33:36 +0100

    make fen derive from std::string

--- include/chester/engine/fen.hpp
+++ include/chester/engine/fen.hpp
@@ -1,17 +1,21 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <utility>
 
 namespace chester::engine {
 
-/**
- * Forsyth-Edwards Notation
- */
-class fen {
+class fen : public std::string {
   public:
-    constexpr explicit fen(std::string string) : string(std::move(string)) {}
-    std::string string;
+    constexpr explicit fen(std::string string)
+        : std::string(std::move(string)) {}
+};
+
+class fen_view : public std::string_view {
+  public:
+    constexpr explicit fen_view(std::string_view view)
+        : std::string_view(view) {}
 };
 
 } // namespace chester::engine
