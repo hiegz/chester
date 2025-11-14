b0a0adc1cd0ad381b31911291d1499d607a4dd02
Volodymyr But <git@hiegz.com>
2025-11-14 16:41:04 +0100

    remove unused #include's

--- tests/engine/bitset.cpp
+++ tests/engine/bitset.cpp
@@ -9,8 +9,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 #include <catch2/generators/catch_generators_range.hpp>
-#include <catch2/generators/catch_generators_adapters.hpp>
-#include <catch2/generators/catch_generators_random.hpp>
 
 using chester::engine::bitset;
 using chester::engine::square;
